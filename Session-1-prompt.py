ae34550e6b62a90cc87212bf7ec3e8e5651700ec
carloselizondo610 <147549772+carloselizondo610@users.noreply.github.com>
2023-10-18 17:06:15 -0700

    Session 1 prompt for Coding Journal 1.

---
+++ Session-1-prompt.py
@@ -0,0 +1,26 @@
+def PrintCarlosElizondo():
+	print("Carlos Elizondo")
+
+def main():
+	PrintCarlosElizondo()
+
+if __name__=="__main__":
+	main()
+
+def PrintRagingBull():
+	print("Raging Bull is my favorite movie. Robbie Robertson's scoring is beautiful and Robert DeNiro's acting is superb.")
+
+def main():
+	PrintRagingBull()
+
+if __name__=="__main__":
+	main()
+
+def PrintTacosdePastor():
+	print("Tacos de pastor son mis comidas favoritas.")
+
+def main():
+	PrintTacosdePastor()
+
+if __name__=="__main__":
+	main()
